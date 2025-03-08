dd87b8f8d1158c556714c3807109464e1d0ad30b
NivethaSre <71762233028@cit.edu.in>
2025-03-08 20:02:03 +0530

    Delete secret.js

--- secret.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    jwtSecret: "your secret key",
-    mongourl: process.env.MONGO_URI || "mongodb+srv://nivi:0000@cluster0.nv4xs.mongodb.net/crowd?retryWrites=true&w=majority"
-};
