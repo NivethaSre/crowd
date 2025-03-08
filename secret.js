ff90b419b1c7515406343f2b8c18d2e617fc8f0b
NivethaSre <71762233028@cit.edu.in>
2025-03-08 19:57:45 +0530

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
 module.exports = {
     jwtSecret: "your secret key",
-    mongourl: process.env.MONGO_URI || "mongodb+srv://Mouli:0000@cluster0.nv4xs.mongodb.net/crowd?retryWrites=true&w=majority"
+    mongourl: process.env.MONGO_URI || "mongodb+srv://nivi:0000@cluster0.nv4xs.mongodb.net/crowd?retryWrites=true&w=majority"
 };
